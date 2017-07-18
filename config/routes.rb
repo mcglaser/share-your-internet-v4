383fd850e94786cc9ea7b0a8348b14e5a4946f43
mcglaser <michaelcharlesglaser@gmail.com>
2017-07-18 13:38:56 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,37 @@
+Rails.application.routes.draw do
+
+
+  root 'static_pages#home'
+
+  get 'signup' => 'users#new'
+
+  get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+  
+  delete 'logout' => 'sessions#destroy'
+
+  get 'average' => 'users#average_internet'
+
+  get 'emp' => 'static_pages#employee'
+
+  get '/auth/facebook/callback' => 'employees#create'
+
+  resources :users do
+      post 'in_it', action: :in_it
+      resources :rolodexes
+      resources :messages
+  end
+
+  resources :communities
+  resources :rolodexes
+  resources :referrals
+  resources :employees
+  resources :messages
+
+
+
+
+end
